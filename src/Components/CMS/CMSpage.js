b24c9714e00f1f87b42e0187add76a8241c95c68
Muniba Hina <muniba.fjwu@gmail.com>
2022-09-16 13:06:17 +0500

    WIP

--- src/Components/CMS/CMSpage.js
+++ src/Components/CMS/CMSpage.js
@@ -6,34 +6,40 @@ import Pageloader from "../Common/Pageloader";
 import { CmsServices } from "./CmsServices";
 
 const CMSpage = (props) => {
-  const [showLoader, setShowLoader] = useState(false);
+  const [showLoader, setShowLoader] = useState(true);
   const [title, setTitle] = useState(null);
   const [data, setData] = useState(null);
   const navigate = useNavigate();
-  const id=props.id;
+  const id = props.id;
   const getPageContent = () => {
     CmsServices.getPageContent(id).then((res) => {
       setShowLoader(false);
       if (!res.isAxiosError && res?.page?.content) {
-        setTitle(res.page.title)
+        setTitle(res.page.title);
         setData(res.page.content);
       } else navigate("/home");
     });
   };
   useEffect(() => {
-   window.scrollTo(0, 0);
+    setShowLoader(true);
+    window.scrollTo(0, 0);
     getPageContent();
-     },[props])
+  }, [props]);
   return (
     <div className="cms-page">
-      {showLoader && <Pageloader />}
-      <h1 className="title">{title}</h1>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: data,
-        }}
-        id="main"
-      ></div>
+      {showLoader ? (
+        <Pageloader />
+      ) : (
+        <>
+          <h1 className="title">{title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: data,
+            }}
+            id="main"
+          ></div>
+        </>
+      )}
     </div>
   );
 };
